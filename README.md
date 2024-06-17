aed179e020b18ccb9488db86fd8dc16c03d2ae33
larryVSC <larrybrown901120@gmail.com>
2024-06-17 09:18:15 +0800

    complete download dataset script and model backbone Unet

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Pre-DDPM
 Pre-DDPM: Exploiting Deep Image Prior for Boosted Sampling Efficiency in Denoising Diffusion Probabilistic Models
+
+
+
+
+## Acknowledgements 
+* [PyTorch Deep Image Prior] (https://github.com/atiyo/deep_image_prior)
+* [DDPM from scratch in Pytorch] (https://www.kaggle.com/code/vikramsandu/ddpm-from-scratch-in-pytorch)
+* [chatGPT] (https://chatgpt.com/)
---
+++ scripts/download_data.sh
@@ -0,0 +1,12 @@
+# create data/ directory
+mkdir -p data
+
+# export kaggle personal tokens
+export KAGGLE_KEY=$(grep '^KAGGLE_KEY=' .env | cut -d '=' -f2-)
+export KAGGLE_USERNAME=$(grep '^KAGGLE_USERNAME=' .env | cut -d '=' -f2-)
+
+# # download
+# $download_path=$(grep '^DOWNLOAD_PATH=' .env | cut -d '=' -f2-)
+
+kaggle datasets download phucthaiv02/butterfly-image-classification/ -p data/ --unzip
+sleep 2
